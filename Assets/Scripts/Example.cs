5b35b7faa4c7754668a6ed7750e6ac6a78f809cd
IT-VRBOX3\Nick <nsaunderswps@gmail.com>
2018-08-03 16:29:36 -0400

    more tests

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float number;
+	public Vector3 rbAngularVelocity;
+
+
+	void Start () {
+
+	}
+	
+
+	void Update () {
+
+
+		rb.gameObject.transform.Translate (0, number*Time.deltaTime,0);
+		/*rb.maxAngularVelocity = 10000000;
+		rb.AddRelativeTorque(0,number,0);
+		rbAngularVelocity = rb.angularVelocity;*/
+	}
+
+	void Blah(float anotherNumber){
+
+		number = anotherNumber;
+
+	}
+}
