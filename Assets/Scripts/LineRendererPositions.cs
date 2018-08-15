6bdeca755e15d5bb2835d89a405d011ace88ff03
IT-VRBOX3\Nick <nsaunderswps@gmail.com>
2018-08-15 17:28:34 -0400

    manywords

---
+++ Assets/Scripts/LineRendererPositions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererPositions : MonoBehaviour {
+
+	LineRenderer line;
+	public GameObject target;
+
+	void Start () {
+
+		line = GetComponent<LineRenderer> ();
+	}
+
+	void Update () {
+		line.SetPosition(0, transform.position);
+		line.SetPosition(1, target.transform.position);
+
+	}
+}
