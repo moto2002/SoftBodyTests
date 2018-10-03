a91204188fa14e7e236f0d39dfe52ce27df5d7eb
IT-VRBOX3\Nick <nsaunderswps@gmail.com>
2018-10-03 17:10:03 -0400

    Clean Up

---
+++ Assets/Scripts/SpaceGravTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceGravTests : MonoBehaviour {
+
+	public GameObject target;
+	Rigidbody rb;
+	public float jumpForce;
+	public float moveForce;
+	float x;
+	float y;
+
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+		
+	}
+	
+
+	void Update () {
+		transform.LookAt (target.transform);
+		x = Input.GetAxis ("Horizontal") * moveForce;
+		y = Input.GetAxis ("Vertical") * moveForce;
+
+		if (Input.GetButtonDown ("Jump")) {
+
+			rb.AddRelativeForce (Vector3.forward * jumpForce);
+		}
+
+	}
+	void FixedUpdate(){
+
+		rb.AddRelativeForce (Vector3.right* y);
+	}
+}
